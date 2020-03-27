5dddd2243348f5e681927a6527a8634a6fa77040
Derik S. Truyts <57511553+deriktruyts@users.noreply.github.com>
2020-03-27 19:35:04 -0300

    Add files via upload

---
+++ CriandoTabela.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS `pais`;
+
+USE `pais`;
+
+CREATE TABLE IF NOT EXISTS `pais`.`paises`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(100) NOT NULL,
+    `populacao` BIGINT NOT NULL,
+    `area` DECIMAL(15,2) NOT NULL,
+    PRIMARY KEY (`id`)
+);
