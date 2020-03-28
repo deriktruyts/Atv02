c36a9bc3c849e623b2d9497bd4c75bf9b7750d3c
Derik S. Truyts <57511553+deriktruyts@users.noreply.github.com>
2020-03-27 21:14:56 -0300

    Add files via upload

---
+++ InserindoNaTabela.sql
@@ -0,0 +1,7 @@
+USE pais;
+
+INSERT INTO paises (nome, populacao, area) VALUES ('Alemanha', 82800000, 357051.00);
+INSERT INTO paises (nome, populacao, area) VALUES ('Itália', 60317546, 301340.00);
+INSERT INTO paises (nome, populacao, area) VALUES ('Russia', 146745098, 17098246.00);
+INSERT INTO paises (nome, populacao, area) VALUES ('China', 1401812360, 9569961.00);
+INSERT INTO paises (nome, populacao, area) VALUES ('India', 1359933123, 3287263.00);
